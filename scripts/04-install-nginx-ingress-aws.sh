2b911668b2052a9220b68e62de0c504159e3f60f
Shivam <shivamekale07@gmail.com>
2026-06-16 14:20:22 +0530

    Updated Str

--- scripts/04-install-nginx-ingress-aws.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.11.3/deploy/static/provider/aws/deploy.yaml
-
-echo "Waiting for ingress controller..."
-kubectl get pods -n ingress-nginx
-kubectl get svc -n ingress-nginx
